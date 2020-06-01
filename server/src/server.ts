0b34b05635c2251aedb2f83a5b26ea4a2842dace
DESKTOP-INVG9UQ\oroma <oromar.melo@gmail.com>
2020-06-01 16:11:24 -0300

    Web App

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const PORT: number = 3333
 const app = express()
 app.get('/users', (req, res) => {
-  res.json(['User1', 'User2', 'User3', 'User4'])
+  res.send('Hello World!!!')
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
